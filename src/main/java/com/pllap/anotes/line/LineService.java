2adee72838dd18b4738d09d3ee499845d82f03c9
Hyeokwoo Kwon <pllapallpal@gmail.com>
2022-01-01 21:03:46 +0900

    Add LineService

---
+++ src/main/java/com/pllap/anotes/line/LineService.java
@@ -0,0 +1,33 @@
+/*
+ * Author : Hyeokwoo Kwon
+ * Filename : LineService.java
+ * Desc :
+ */
+
+package com.pllap.anotes.line;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class LineService {
+    private final LineRepository lineRepository;
+
+    public LineService(LineRepository lineRepository) {
+        this.lineRepository = lineRepository;
+    }
+
+    @Transactional
+    public Optional<Line> save(String content) {
+        Line line = Line.builder()
+                .content(content)
+                .createdDateTime(LocalDateTime.now())
+                .updatedDateTime(LocalDateTime.now())
+                .build();
+        Line savedLine = lineRepository.save(line);
+        return Optional.of(savedLine);
+    }
+}
