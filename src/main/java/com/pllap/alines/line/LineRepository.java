994939a4801e0e9f47fec0ab4108032b56deb409
Hyeokwoo Kwon <pllapallpal@gmail.com>
2022-01-07 23:57:01 +0900

    Update LineRepository

--- src/main/java/com/pllap/alines/line/LineRepository.java
+++ src/main/java/com/pllap/alines/line/LineRepository.java
@@ -9,8 +9,20 @@ package com.pllap.alines.line;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface LineRepository extends JpaRepository<Line, Long> {
+
     @Override
     <S extends Line> S save(S entity);
+
+    @Override
+    List<Line> findAll();
+
+    Optional<Line> findByHash(String hash);
+
+    @Override
+    void delete(Line entity);
 }
