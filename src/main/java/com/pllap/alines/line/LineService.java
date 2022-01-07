0c016d23e5e46df761483185b45cf10ab0f6bc07
Hyeokwoo Kwon <pllapallpal@gmail.com>
2022-01-08 00:02:17 +0900

    Update LineService

--- src/main/java/com/pllap/alines/line/LineService.java
+++ src/main/java/com/pllap/alines/line/LineService.java
@@ -10,10 +10,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class LineService {
+
     private final LineRepository lineRepository;
 
     public LineService(LineRepository lineRepository) {
@@ -21,13 +22,31 @@ public class LineService {
     }
 
     @Transactional
-    public Optional<Line> save(String content) {
+    public Line createLine(String content) {
         Line line = Line.builder()
                 .content(content)
                 .createdDateTime(LocalDateTime.now())
                 .updatedDateTime(LocalDateTime.now())
                 .build();
-        Line savedLine = lineRepository.save(line);
-        return Optional.of(savedLine);
+        return lineRepository.save(line);
+    }
+
+    @Transactional
+    public List<Line> findAll() {
+        return lineRepository.findAll();
+    }
+
+    @Transactional
+    public Line updateLine(String hash, String content) {
+        Line line = lineRepository.findByHash(hash).orElseThrow();
+        line.setContent(content);
+        line.setUpdatedDateTime(LocalDateTime.now());
+        return lineRepository.save(line);
+    }
+
+    @Transactional
+    public void deleteLine(String hash) {
+        Line line = lineRepository.findByHash(hash).orElseThrow(); //TODO: throw and handle exception
+        lineRepository.delete(line);
     }
 }
